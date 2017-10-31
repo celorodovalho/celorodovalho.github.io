803df3c13b322fe9a18af25fa84d643faff543af
Ceelo <marcelo2208@gmail.com>
2017-10-31 14:22:13 -0200

    Update Main.js

--- js/controllers/Main.js
+++ js/controllers/Main.js
@@ -5,7 +5,7 @@ var appModule = angular.module('app');
 appModule.controller('MainController', ['$scope', '$location', '$window', function ($scope, $location, $window) {
     $scope.year = new Date().getFullYear();
     $scope.mail = 'contato@marcelorodovalho.com';
-    $scope.phone = '+55 61 99302 5413';
+    $scope.phone = '+55 61 991 852 247';
     $scope.social = {
         facebook: 'https://www.facebook.com/MarceloRodovalhoDeveloper',
         linkedin: 'https://br.linkedin.com/in/marcelorodovalho',
